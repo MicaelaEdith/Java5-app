8d5354fc482f361c1a50ca9431c8533131316f49
Micaela Edith <romeromicaelaedith@gmail.com>
2024-01-08 21:15:25 -0300

    seteo maven

---
+++ app-java5-console/src/main/java/org/app/java5/console/App.java
@@ -0,0 +1,13 @@
+package org.app.java5.console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
